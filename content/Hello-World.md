772c2a2a1445935ea3211dd718a3037c8f09175f
douglask3 - Reading <douglas.i.kelley@gmail.com>
2015-09-29 19:50:12 +0100

    fixing library to pass variable as commit message propely, and updating page to demostrate test

--- content/Hello-World.md
+++ content/Hello-World.md
@@ -1,5 +1,5 @@
 Title: My First Review
 Date: 2010-12-03 10:20
-Category: Review
+Category: Review yay
 
 Following is a review of my favorite mechanical keyboard.
--- libs/push2website.sh
+++ libs/push2website.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env sh
 make html
+pwd
 rev=$(git rev-parse HEAD)
 url=$(git remote show origin)
 mss="genrated website from rev no. "${rev}" "${url}
+echo $mss
 cd output
-git commit -am  $mss#
+git add .
+git commit -m  "${mss}"
 git push
 cd ..
