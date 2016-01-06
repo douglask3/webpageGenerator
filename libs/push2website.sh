5762cc4b80b0cd2dd39f5d8cdd89b7a5088c9382
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-01-06 17:39:24 +0000

    adding template git info to website commit mess

--- libs/push2website.sh
+++ libs/push2website.sh
@@ -3,7 +3,15 @@
 pwd
 rev=$(git rev-parse HEAD)
 url=$(git remote show origin)
-mss="genrated website from rev no. "${rev}" "${url}
+mss1="genrated website from rev no. "${rev}" "${url}
+
+cd html5-dopetrope
+rev=$(git rev-parse HEAD)
+url=$(git remote show origin)
+mss2="using template "${rev}" "${url}
+cd ..
+
+mss=${mss1}"\n"${mss2}
 echo $mss
 cd output
 git add .
