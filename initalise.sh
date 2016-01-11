61aea402a66303d2bba783b46bf440ef5106da5c
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-01-11 18:17:34 +0000

    running google shcolar grab initailaisatipn

--- initalise.sh
+++ initalise.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 ./libs/gitCloneRep.sh https://github.com/douglask3/googleScholarGrab.git "libs/googleScholarGrab/"
 
-
 cd libs/googleScholarGrab/
-git pull -u origin master
+    git pull -u origin master
+    ./initialise.r
 cd ../..
