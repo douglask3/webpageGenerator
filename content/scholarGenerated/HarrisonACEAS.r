b00087008c46bd584c60c42bef3373c2a524835c
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-01-12 09:59:03 +0000

    adding ACEAS poster

---
+++ content/scholarGenerated/HarrisonACEAS.r
@@ -0,0 +1,44 @@
+header =
+'Title: Using plant functional traits to predict ecosystem vulnerability to changing fire regimes
+Date: 2013-08-01 00:00
+Category: Publications
+status: published
+tag: Publications
+
+Australia lacks a coherent approach for synthesizing plant functional traits to
+predict the response of ecosystems to
+changing fire regimes. Large plant community datasets already exist for major
+ecosystems in Australia, from rainforests
+to deserts. This working group aimed to attribute species into fire persistence
+groups, with the objective of predicting
+the persistence (bud resprouting type) of the species comprising a community in
+relation to resource gradients and
+fire regimes using a new scheme, the Buds-Protection-Resource scheme (BPR)
+(Clarke et al. 2013). Our analyses
+focussed on creating predictive models with accompanying levels of confidence.
+Our working group added value to
+the work of a previous ACEAS working group (Murphy et al. 2011; Bowman et al.
+    2013; Murphy et al. 2013) by linking
+their predicted changes in fire regimes to biodiversity effects. Our analyses
+incorporate an understanding of plant fire
+resilience traits and the role of the persistence niche into the whole of
+continent approach developed previously.
+
+'
+
+cnameExtra  = '<hr> &nbsp;'
+cnameFormat = c('&nbsp; <h3 class = "publication">', '</h3> &nbsp;')
+pubSep      = '<hr>'
+titleFormat = c('<h3 class = "publication">', '</h3>')
+citeFormat  = c('&nbsp;', '')
+yearFormat  = c('&nbsp;', '')
+textFormat  = c('', '')
+
+footer = ''
+
+outputFile = 'content/publications/HarrisonACEAS.md'
+
+MakePublicationDocument(c('Douglas Kelley' = 'AJKyfI4AAAAJ'), FALSE,
+                        header, cnameExtra, cnameFormat,
+                        pubSep, titleFormat, citeFormat, yearFormat, textFormat,
+                        footer, outputFile, 'u5HHmVD_uO8C')
