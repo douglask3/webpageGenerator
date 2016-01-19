d91fffdc77b3808f86017865f59d764a4037210a
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-01-19 15:39:35 +0000

    fixing make tool files so generated files and added to .gitignore. And adding the change in .gitignore to include generated tool files

--- libs/makeToolPages.r
+++ libs/makeToolPages.r
@@ -6,8 +6,9 @@ MakeToolDocument <- function(header, file, footer, outputFile) {
     print(file)
     file = getURL("https://raw.githubusercontent.com/douglask3/cGDAY/master/README.rst")
     out  = paste(c(header, file, footer), collapse = "\n\n")
-    print(outputFile)
+
     cat(out, file = outputFile, sep = "\n")
+    return(outputFile)
 }
 
 GenerateRfiles(ignoreLine = '## Tool Generated Files Automatically added between these two lines ##',
