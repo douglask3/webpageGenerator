99893942cfd37ebfb19ec7f954f28d576b9b4519
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-01-29 15:35:05 +0000

    benchmarking metrics tool page

---
+++ content/repoPageGenerated/vegetation_model_benchmarking.r
@@ -0,0 +1,18 @@
+
+header =
+'Title: Vegetation Model Benchmarking
+date: 2000-01-03
+Category: Tools
+status: published
+tags: Tools
+Template: article_elsewhere
+summary: <div class="github-card" data-user="douglask3" data-repo="benchmarkmetrics" data-target="blank" data-width = "100%"></div><script src="//cdn.jsdelivr.net/github-cards/latest/widget.js"></script>
+'
+
+file = "https://raw.githubusercontent.com/douglask3/benchmarkmetrics/master/README.md"
+
+footer = ''
+
+outputFile = 'content/tools/vegetation_model_benchmarking.md'
+
+MakeToolDocument(header, file, footer, outputFile)
