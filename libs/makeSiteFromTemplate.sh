d446c2325ac2d5b465e78c6d8397331d3a0efb00
douglask3 - Reading <douglas.i.kelley@gmail.com>
2016-02-25 10:42:50 +0000

    moving some important pages to root

--- libs/makeSiteFromTemplate.sh
+++ libs/makeSiteFromTemplate.sh
@@ -4,3 +4,6 @@
 pelican content -s pelicanconf.py -t html5-dopetrope
 
 ./libs/makeCV.r
+cp output/pages/research-interests.html output/research-interests.html
+cp output/pages/publications.html output/publications.html
+cp output/pages/tools-and-tutorials.html output/tools.html
