567acab83c4bfd72526eb247969775e552d5e775
douglask3 - Reading <douglas.i.kelley@gmail.com>
2017-02-07 21:35:01 +0000

    switiching template

--- content/repoPageGenerated/vegetation_model_benchmarking.r
+++ content/repoPageGenerated/vegetation_model_benchmarking.r
@@ -5,7 +5,7 @@ date: 2000-01-03
 Category: Tools
 status: published
 tags: Tools and Tutorials
-Template: article_elsewhere
+Template: article_feed
 picture: http://douglask3.github.io/images/Benchmarking-Fig1.png
 summary: R package containing sepcifically design metrics to assess and benchmark Land Surface and Vegetation Models against observations, but can also be applied to any simulation vs observation comparison.
 '
